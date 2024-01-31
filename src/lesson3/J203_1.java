a873d3ac86a988c5123542a166e720e695f3393d
JoshuaT <jttsat170@gmail.com>
2024-01-31 11:20:16 -0500

    exception handling part1

---
+++ src/lesson3/J203_1.java
@@ -0,0 +1,25 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class J203_1 {
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+        System.out.println("Please enter two numbers");
+     int num = input.nextInt();
+     int num2 = input.nextInt();
+        divideMethod(num, num2);
+
+    }
+
+    static double divideMethod(int num, int num2){
+       try{
+        return num / num2;
+       }catch (Exception ex){
+           System.out.println("You can't by zero");
+       }finally {
+           System.out.println("This program still is running");
+       }
+       return 0.0;
+    }
+}
